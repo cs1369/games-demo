a7dfe634982d125097f6f3982cb6599fbb546ae8
cs1369 <suarezgabriel420@gmail.com>
2022-06-27 22:05:39 -0300

    prueba1

---
+++ js/app.js
@@ -0,0 +1,684 @@
+const palabras = [{
+    id: 1,
+    wordIng: "accept",
+    wordEsp:"aceptar",
+},
+{
+    id: 2,
+    wordIng: "act",
+    wordEsp:"actuar",
+},
+{
+    id: 3,
+    wordIng: "bake",
+    wordEsp:"endurecer",
+},
+{
+    id: 4,
+    wordIng: "behave",
+    wordEsp:"comportarse",
+},
+{
+    id: 5,
+    wordIng: "close",
+    wordEsp:"cerrar",
+},
+{
+    id: 6,
+    wordIng: "compare",
+    wordEsp:"comparar",
+},
+{
+    id: 7,
+    wordIng: "compete",
+    wordEsp:"competir",
+},
+{
+    id: 8,
+    wordIng: "die",
+    wordEsp:"morir",
+},
+{
+    id: 9,
+    wordIng: "disagree",
+    wordEsp:"discrepar",
+},
+{
+    id: 10,
+    wordIng: "disturb",
+    wordEsp:"molestar",
+},
+{
+    id: 11,
+    wordIng: "dress",
+    wordEsp:"vestirse",
+},
+{
+    id: 12,
+    wordIng: "dry",
+    wordEsp:"secar",
+},
+{
+    id: 13,
+    wordIng: "eliminate",
+    wordEsp:"eliminar",
+},
+{
+    id: 14,
+    wordIng: "end",
+    wordEsp:"terminar"
+},
+{
+    id: 15,
+    wordIng: "enjoy",
+    wordEsp:"disfrutar"
+},
+{
+    id: 16,
+    wordIng: "fix",
+    wordEsp:"arreglar"
+},
+{
+    id: 17,
+    wordIng: "follow",
+    wordEsp:"seguir"
+},
+{
+    id: 18,
+    wordIng: "freeze",
+    wordEsp:"congelar"
+},
+{
+    id: 19,
+    wordIng: "fry",
+    wordEsp:"freir"
+},
+{
+    id: 20,
+    wordIng: "greet",
+    wordEsp:"saludar"
+},
+{
+    id: 21,
+    wordIng: "guess",
+    wordEsp:"adivinar"
+},
+{
+    id: 22,
+    wordIng: "hunt",
+    wordEsp:"cazar"
+},
+{
+    id: 23,
+    wordIng: "identify",
+    wordEsp:"identificar"
+},
+{
+    id: 24,
+    wordIng: "imagine",
+    wordEsp:"imaginar"
+},
+{
+    id: 25,
+    wordIng: "invite",
+    wordEsp:"invitar"
+},
+{
+    id: 26,
+    wordIng: "join",
+    wordEsp:"unirse"
+},
+{
+    id: 27,
+    wordIng: "jump",
+    wordEsp:"saltar"
+},
+{
+    id: 28,
+    wordIng: "knock",
+    wordEsp:"golpear"
+},
+{
+    id: 29,
+    wordIng: "love",
+    wordEsp:"amar"
+},
+{
+    id: 30,
+    wordIng: "manage",
+    wordEsp:"administrar"
+},
+{
+    id: 31,
+    wordIng: "mark",
+    wordEsp:"marcar"
+},
+{
+    id: 32,
+    wordIng: "match",
+    wordEsp:"igualar"
+},
+{
+    id: 33,
+    wordIng: "name",
+    wordEsp:"nombrar"
+},
+{
+    id: 34,
+    wordIng: "need",
+    wordEsp:"necesitar"
+},
+{
+    id: 35,
+    wordIng: "open",
+    wordEsp:"abrir"
+},
+{
+    id: 36,
+    wordIng: "order",
+    wordEsp:"ordenar"
+},
+{
+    id: 37,
+    wordIng: "organize",
+    wordEsp:"organizar"
+},
+{
+    id: 38,
+    wordIng: "pack",
+    wordEsp:""
+},
+{
+    id: 39,
+    wordIng: "paint",
+    wordEsp:"pintar"
+},
+{
+    id: 40,
+    wordIng: "pass",
+    wordEsp:"aprobar"
+},
+{
+    id: 41,
+    wordIng: "perform",
+    wordEsp:"realizar"
+},
+{
+    id: 42,
+    wordIng: "persuade",
+    wordEsp:"persuadir"
+},
+{
+    id: 43,
+    wordIng: "program",
+    wordEsp:"programar"
+},
+{
+    id: 44,
+    wordIng: "protect",
+    wordEsp:"proteger"
+},
+{
+    id: 45,
+    wordIng: "review",
+    wordEsp:"examinar"
+},
+{
+    id: 46,
+    wordIng: "shop",
+    wordEsp:"hacer compras"
+},
+{
+    id: 47,
+    wordIng: "slow",
+    wordEsp:"retardar"
+},
+{
+    id: 48,
+    wordIng: "turn",
+    wordEsp:"girar"
+},
+{
+    id: 49,
+    wordIng: "underline",
+    wordEsp:""
+},
+{
+    id: 50,
+    wordIng: "want",
+    wordEsp:"desear"
+},
+{
+    id: 51,
+    wordIng: "bring",
+    wordEsp:"traer"
+},
+{
+    id: 52,
+    wordIng: "build",
+    wordEsp:"construir"
+},
+{
+    id: 53,
+    wordIng: "buy",
+    wordEsp:"comprar"
+},
+{
+    id: 54,
+    wordIng: "catch",
+    wordEsp:"capturar"
+},
+{
+    id: 55,
+    wordIng: "feel",
+    wordEsp:"sentir"
+},
+{
+    id: 56,
+    wordIng: "find",
+    wordEsp:"encontrar"
+},
+{
+    id: 57,
+    wordIng: "fight",
+    wordEsp:"pelear"
+},
+{
+    id: 58,
+    wordIng: "get",
+    wordEsp:"obtener"
+},
+{
+    id: 59,
+    wordIng: "have",
+    wordEsp:"tener"
+},
+{
+    id: 60,
+    wordIng: "hear",
+    wordEsp:"escuchar"
+},
+{
+    id: 61,
+    wordIng: "hold",
+    wordEsp:"sostener"
+},
+{
+    id: 62,
+    wordIng: "keep",
+    wordEsp:"mantener"
+},
+{
+    id: 63,
+    wordIng: "leave",
+    wordEsp:"salir"
+},
+{
+    id: 64,
+    wordIng: "lend",
+    wordEsp:"prestar"
+},
+{
+    id: 65,
+    wordIng: "light",
+    wordEsp:"iluminar"
+},
+{
+    id: 66,
+    wordIng: "lose",
+    wordEsp:"perder"
+},
+{
+    id: 67,
+    wordIng: "make",
+    wordEsp:"hacer"
+},
+{
+    id: 68,
+    wordIng: "meet",
+    wordEsp:"conocer"
+},
+{
+    id: 69,
+    wordIng: "pay",
+    wordEsp:"pagar"
+},
+{
+    id: 70,
+    wordIng: "read",
+    wordEsp:"leer"
+},
+{
+    id: 71,
+    wordIng: "say",
+    wordEsp:"decir"
+},
+{
+    id: 72,
+    wordIng: "sell",
+    wordEsp:"vender"
+},
+{
+    id: 73,
+    wordIng: "send",
+    wordEsp:"enviar"
+},
+{
+    id: 74,
+    wordIng: "shoot",
+    wordEsp:"disparar"
+},
+{
+    id: 75,
+    wordIng: "sit",
+    wordEsp:"sentarse"
+},
+{
+    id: 76,
+    wordIng: "sleep",
+    wordEsp:"dormir"
+},
+{
+    id: 77,
+    wordIng: "spend",
+    wordEsp:"gastar"
+},
+{
+    id: 78,
+    wordIng: "stand",
+    wordEsp:"pararse"
+},
+{
+    id: 79,
+    wordIng: "teach",
+    wordEsp:"enseñar"
+},
+{
+    id: 80,
+    wordIng: "think",
+    wordEsp:"pensar"
+},
+{
+    id: 81,
+    wordIng: "tell",
+    wordEsp:"contar"
+},
+{
+    id: 82,
+    wordIng: "win",
+    wordEsp:"ganar"
+},
+{
+    id: 83,
+    wordIng: "bite",
+    wordEsp:"morder"
+},
+{
+    id: 84,
+    wordIng: "break",
+    wordEsp:"romper"
+},
+{
+    id: 85,
+    wordIng: "choose",
+    wordEsp:"elegir"
+},
+{
+    id: 86,
+    wordIng: "fly",
+    wordEsp:"volar"
+},
+{
+    id: 87,
+    wordIng: "forget",
+    wordEsp:"olvidar"
+},
+{
+    id: 88,
+    wordIng: "hide",
+    wordEsp:"esconder"
+},
+{
+    id: 89,
+    wordIng: "lie",
+    wordEsp:"mentir"
+},
+{
+    id: 90,
+    wordIng: "steal",
+    wordEsp:"robar"
+},
+{
+    id: 91,
+    wordIng: "speak",
+    wordEsp:"hablar"
+},
+{
+    id: 92,
+    wordIng: "tear",
+    wordEsp:"rasgar"
+},
+{
+    id: 93,
+    wordIng: "wake",
+    wordEsp:"despertar"
+},
+{
+    id: 94,
+    wordIng: "wear",
+    wordEsp:"usar"
+},
+{
+    id: 95,
+    wordIng: "do",
+    wordEsp:"hacer"
+},
+{
+    id: 96,
+    wordIng: "draw",
+    wordEsp:"dibujar"
+},
+{
+    id: 97,
+    wordIng: "drive",
+    wordEsp:"manejar"
+},
+{
+    id: 98,
+    wordIng: "eat",
+    wordEsp:"comer"
+},
+{
+    id: 99,
+    wordIng: "fall",
+    wordEsp:"caer"
+},
+{
+    id: 100,
+    wordIng: "give",
+    wordEsp:"dar"
+},
+{
+    id: 101,
+    wordIng: "go",
+    wordEsp:"ir"
+},
+{
+    id: 102,
+    wordIng: "grow",
+    wordEsp:"crecer"
+},
+{
+    id: 103,
+    wordIng: "know",
+    wordEsp:"saber"
+},
+{
+    id: 104,
+    wordIng: "ride",
+    wordEsp:"montar"
+},
+{
+    id: 105,
+    wordIng: "rise",
+    wordEsp:"subir"
+},
+{
+    id: 106,
+    wordIng: "see",
+    wordEsp:"ver"
+},
+{
+    id: 107,
+    wordIng: "show",
+    wordEsp:"demostrar"
+},
+{
+    id: 108,
+    wordIng: "take",
+    wordEsp:"tomar"
+},
+{
+    id: 109,
+    wordIng: "write",
+    wordEsp:"escribir"
+},
+{
+    id: 110,
+    wordIng: "begin",
+    wordEsp:"comenzar"
+},
+{
+    id: 111,
+    wordIng: "drink",
+    wordEsp:"beber"
+},
+{
+    id: 112,
+    wordIng: "ring",
+    wordEsp:"llamar"
+},
+{
+    id: 113,
+    wordIng: "sing",
+    wordEsp:"cantar"
+},
+{
+    id: 114,
+    wordIng: "sink",
+    wordEsp:"hundir"
+},
+{
+    id: 115,
+    wordIng: "swim",
+    wordEsp:"nadar"
+},
+{
+    id: 116,
+    wordIng: "become",
+    wordEsp:"convertirse en"
+},
+{
+    id: 117,
+    wordIng: "come",
+    wordEsp:"venir"
+},
+{
+    id: 118,
+    wordIng: "run",
+    wordEsp:"correr"
+},
+{
+    id: 119,
+    wordIng: "bet",
+    wordEsp:"apostar"
+},
+{
+    id: 120,
+    wordIng: "cost",
+    wordEsp:"costar"
+},
+{
+    id: 121,
+    wordIng: "cut",
+    wordEsp:"cortar"
+},
+{
+    id: 122,
+    wordIng: "hit",
+    wordEsp:"golpear"
+},
+{
+    id: 123,
+    wordIng: "hurt",
+    wordEsp:"herir"
+},
+{
+    id: 124,
+    wordIng: "let",
+    wordEsp:"alquilar"
+},
+{
+    id: 125,
+    wordIng: "put",
+    wordEsp:"poner"
+},
+{
+    id: 126,
+    wordIng: "shut",
+    wordEsp:"cerrar"
+}
+];
+
+const verificar = document.querySelector(".verificar");
+const seguir = document.querySelector(".seguir");
+const juegoPalabras = document.querySelector(".juego__palabra");
+const juegotxt = document.querySelector(".juego__input");
+let numeroX = -1;
+let estado;
+seguir.addEventListener("click",()=>{
+    let numRandom = palabraRandom(palabras.length);
+    seguir.textContent="Otra Palabra";
+    juegotxt.value="";
+    document.querySelector(".resultado").textContent="";
+
+    if(numeroX > numRandom || numeroX === numRandom){
+        juegoPalabras.textContent=palabraMayus(palabras[numRandom].wordEsp);
+        document.querySelector(".respuesta__idioma").textContent = "Ingles";
+        numeroX=numRandom;
+        estado=true;
+    }
+    else{
+        juegoPalabras.textContent=palabraMayus(palabras[numRandom].wordIng);
+        document.querySelector(".respuesta__idioma").textContent = "Español";
+        numeroX=numRandom;
+        estado=false;
+    }
+});
+verificar.addEventListener("click",()=>{
+    if(estado){
+        if(palabras[numeroX].wordIng.toLowerCase() == juegotxt.value.toLowerCase()){
+            document.querySelector(".resultado").style.color="#8ac926";
+            document.querySelector(".resultado").textContent="¡Correcto!"
+        }
+        else{
+            document.querySelector(".resultado").style.color="#e63946";
+            document.querySelector(".resultado").textContent="¡Incorrecto!"
+        }
+    }else{
+        if(palabras[numeroX].wordEsp.toLowerCase() === juegotxt.value.toLowerCase()){
+            document.querySelector(".resultado").style.color="#8ac926";
+            document.querySelector(".resultado").textContent="¡Correcto!"
+        }
+        else{
+            document.querySelector(".resultado").style.color="#e63946";
+            document.querySelector(".resultado").textContent="¡Incorrecto!"
+        }
+    } 
+})
+function palabraRandom(tam){
+    return Math.floor(Math.random()*tam);
+}
+function palabraMayus(palabra){
+    return palabra.charAt(0).toUpperCase()+palabra.slice(1);
+}
